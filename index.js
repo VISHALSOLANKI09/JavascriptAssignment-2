32fea36f9f7baa3dc66ec02906e0600851a64af7
VISHALSOLANKI09 <104018474+VISHALSOLANKI09@users.noreply.github.com>
2022-05-03 13:22:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+displayData();
+
+var valid_name, valid_email, valid_number;
+var regexForName = /^([a-zA-Z]+|[a-zA-Z]+\s{1}[a-zA-Z]{1,}|[a-zA-Z]+\s{1}[a-zA-Z]{3,}\s{1}[a-zA-Z]{1,})$/i;
+var regexForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var regexForPhone = /^\d{10}$/;
+
+function saveData() {
+    let input_name,email,phone;
+    valid_name = false;
+    valid_email = false;
+    valid_phone = false;
+    input_name=document.getElementById("name").value;
+    email=document.getElementById("email").value;
+    phone=document.getElementById("phone").value;
+    validateData(input_name, email, phone);
+}
+
+function validateData(input_name, email, phone) {
+    if(input_name.length && regexForName.test(input_name)) {
+        valid_name = true;
+        document.getElementById("invalid_name").style.display = "none";
+        document.getElementById("invalid_name").style.color = "red";
+        document.getElementById("name").style.borderColor = "green";
+    } else {
+        valid_name = false;
+        document.getElementById("invalid_name").style.display = "block";
+        document.getElementById("name").style.borderColor = "red";
+    }
+
+    if(email.length && regexForEmail.test(email)) {
+        valid_email = true;
+        document.getElementById("invalid_email").style.display = "none";
+        document.getElementById("email").style.borderColor = "green";
+    } else {
+        valid_email = false;
+        document.getElementById("invalid_email").style.display = "block";
+        document.getElementById("email").style.borderColor = "red";
+    }
+
+    if(phone.length && regexForPhone.test(phone)) {
+        valid_phone = true;
+        document.getElementById("invalid_phone").style.display = "none";
+        document.getElementById("phone").style.borderColor = "green";
+    } else {
+        valid_phone = false;
+        document.getElementById("invalid_phone").style.display = "block";
+        document.getElementById("phone").style.borderColor = "red";
+    }
+    
+    if(phone.length == 0 || phone.length == 10) {
+        document.getElementById("phone").style.borderColor = "green";
+    }
+
+    // console.log(input_name);
+    // console.log(email);
+    // console.log(phone);
+    // console.log(valid_name);
+    // console.log(valid_phone);
+    // console.log(valid_email);
+
+    if((!valid_name || !valid_email)) {
+        alert("Fill The Details Properly");
+    } else {
+        let data = new Array();
+        data = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
+        if(data.some((value)=> { return value.email==email })) {
+            alert("Duplicate Data.");
+        } else {
+            data.push({
+                "name": input_name,
+                "email": email,
+                "phone": phone
+            });
+            localStorage.setItem("users", JSON.stringify(data));
+        }
+        displayData();
+    }
+}
+
+function displayData() {
+    document.getElementById("displayData").innerHTML = "";
+    let records = new Array();
+    records = JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
+    if(records) {
+        for(let i = 0; i < records.length; i++) {
+            let new_div = document.createElement("tr");
+
+            new_div.innerHTML = `<tr>
+                <td>${records[i].name}</td>
+                <td>${records[i].email}</td>
+                <td>${records[i].phone}</td>
+            `
+            document.getElementById("displayData").append(new_div);
+        }
+    }
+}
+
+
